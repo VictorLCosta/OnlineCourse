498e8a70becc08dc769f3fd7875cb05f092f45e6
VictorLCosta <victorlc2019@outlook.com>
2021-09-19 17:04:42 -0300

    Treating invalid params

--- OnlineCourse.Test/FirsTest.cs
+++ OnlineCourse.Test/FirsTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using ExpectedObjects;
 using Xunit;
 
@@ -25,6 +27,68 @@ namespace OnlineCourse.Test
 
             expectedCourse.ToExpectedObject().ShouldMatch(course);
         }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void CourseShouldNotHaveAnInvalidName(string invalidName)
+        {
+            var expectedCourse = new
+            {
+                Name = invalidName,
+                Workload = (double)30,
+                TargetAudience = TargetAudience.Student,
+                Value = (double)590
+            };
+
+            var message = Assert.Throws<ArgumentException>(() => 
+                new Course(invalidName, expectedCourse.Workload, expectedCourse.TargetAudience, expectedCourse.Value))
+                .Message;
+
+            Assert.Equal("Invalid name", message);
+        }
+
+        [Theory(DisplayName = "Course Workload Is Not Less Than 1")]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-200)]
+        public void CourseWorkloadMustNotLessThan1(double invalidWorkLoad) {
+
+            var expectedCourse = new
+            {
+                Name = "Informática básica",
+                Workload = invalidWorkLoad,
+                TargetAudience = TargetAudience.Student,
+                Value = (double)590
+            };
+
+            var message = Assert.Throws<ArgumentException>(() => 
+                new Course(expectedCourse.Name, expectedCourse.Workload, expectedCourse.TargetAudience, expectedCourse.Value))
+                .Message;
+
+            Assert.Equal("Invalid workload", message);
+        }
+
+        [Theory(DisplayName = "Course Value Is Not Less Than 1")]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-200)]
+        public void CourseValueMustNotLessThan1(double value) {
+
+            var expectedCourse = new
+            {
+                Name = "Informática básica",
+                Workload = (double)50,
+                TargetAudience = TargetAudience.Student,
+                Value = value
+            };
+
+            var message = Assert.Throws<ArgumentException>(() => 
+                new Course(expectedCourse.Name, expectedCourse.Workload, expectedCourse.TargetAudience, expectedCourse.Value))
+                .Message;
+
+            Assert.Equal("Invalid value", message);
+        }
     }
 
     public enum TargetAudience : int
@@ -39,6 +103,15 @@ namespace OnlineCourse.Test
     {
         public Course(string name, double workload, TargetAudience targetAudience, double value)
         {
+            if(String.IsNullOrEmpty(name))
+                throw new ArgumentException("Invalid name");
+
+            if(workload < 1)
+                throw new ArgumentException("Invalid workload");
+
+            if(value < 1)
+                throw new ArgumentException("Invalid value");
+
             Name = name;
             Workload = workload;
             TargetAudience = targetAudience;
